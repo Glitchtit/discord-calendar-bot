cbdc38af3e40d4e2974b29448426e05356e627f5
Glitchtit <thomas.wredlund@gmail.com>
2025-03-29 21:39:50 +0200

    -

--- environ.py
+++ environ.py
@@ -3,5 +3,5 @@ import os
 DEBUG = os.getenv("DEBUG", "false").lower() == "true"
 DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-GOOGLE_APPLICATION_CREDENTIALS = os.environ.get("service_account.json")
+GOOGLE_APPLICATION_CREDENTIALS = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS", "service_account.json")
 CALENDAR_SOURCES = os.getenv("CALENDAR_SOURCES")
