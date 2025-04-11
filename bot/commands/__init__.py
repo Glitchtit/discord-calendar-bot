b817410c80a5a17e9d8b1bb08f116c3ed4d20866
Glitchtit <thomas.wredlund@gmail.com>
2025-04-11 20:33:28 +0300

    feat: Import handle_herald_command from herald in commands package

--- bot/commands/__init__.py
+++ bot/commands/__init__.py
@@ -1,5 +1,6 @@
 from .herald import post_tagged_events
 from .herald import post_tagged_week
+from .herald import handle_herald_command
 from .agenda import handle_agenda_command
 from .greet import handle_greet_command
 from .reload import handle_reload_command
